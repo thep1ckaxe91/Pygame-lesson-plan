49165f06a3e8d0df91ce657cae2806f0559ac580
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2023-07-12 23:55:00 +0700

    u

--- Pong/main.py
+++ Pong/main.py
@@ -11,6 +11,7 @@ player_width = 20
 player_height = 150
 player_offset_x = 20
 player_speed = 5
+player_1_turn = True
 player_1_rect = pygame.Rect(
     player_offset_x,
     HEIGHT/2-player_height/2,
@@ -23,8 +24,33 @@ player_2_rect = pygame.Rect(
     player_width,
     player_height
 )
-ball_rect = pygame.Rect()
+ball_edge_size = 20
+ball_init_vel = 5
+ball_vel = pygame.math.Vector2(ball_init_vel,0)
 
+ball_rect = pygame.Rect(
+    WIDTH/2-ball_edge_size/2,
+    HEIGHT/2-ball_edge_size/2,
+    ball_edge_size,
+    ball_edge_size
+)
+
+def init_ball():
+    ball_rect.center = (WIDTH/2,HEIGHT/2)
+    if player_1_turn:
+        ball_vel.x = -ball_init_vel
+    else:
+        ball_vel.x = ball_init_vel
+    ball_vel.y = 0
+    ball_vel.rotate_ip(randint(-30,30))
+init_ball()
+
+def collision_handler():
+    if ball_rect.top + ball_vel.y < 0 :
+        ball_rect.x += ball_vel.x
+        ball_rect.y += ball_rect.y-
+        ball_vel.y = -ball_vel.y
+    if ba
 
 def fixedUpdate():
     keys = pygame.key.get_pressed()
@@ -32,15 +58,29 @@ def fixedUpdate():
         player_1_rect.y -= player_speed
     if keys[pygame.K_s]:
         player_1_rect.y += player_speed
+    player_1_rect.clamp_ip(player_1_rect.x,0,player_width,HEIGHT)
     if keys[pygame.K_UP]:
         player_2_rect.y -= player_speed
     if keys[pygame.K_DOWN]:
         player_2_rect.y += player_speed
+    player_2_rect.clamp_ip(player_2_rect.x,0,player_width,HEIGHT)
+    
+    ball_rect.center += ball_vel
+
+    collision_handler()
+
+
+def draw_player():
+    pygame.draw.rect(window,WHITE,player_1_rect)
+    pygame.draw.rect(window,WHITE,player_2_rect)
+
+def draw_ball():
+    pygame.draw.rect(window,WHITE,ball_rect)
 
 def drawScreen():
     window.fill('black')
-    pygame.draw.rect(window,WHITE,player_1_rect)
-    pygame.draw.rect(window,WHITE,player_2_rect)
+    draw_player()
+    draw_ball()
 
 
 if __name__ == "__main__":
