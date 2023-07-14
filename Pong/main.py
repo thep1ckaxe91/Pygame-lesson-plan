172e0bb407a9c85762da71625584b28a53055129
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2023-07-14 19:11:33 +0700

    sad

--- Pong/main.py
+++ Pong/main.py
@@ -1,17 +1,31 @@
 from random import randint
 import pygame,math,sys,time
+from pygame.math import Vector2
 pygame.init()
+pygame.font.init()
+pygame.mixer.init()
+
 WIDTH,HEIGHT = 1366,768
-refresh_rate = 60
+WHITE = (255,255,255)
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
-WHITE = (255,255,255)
+SCORE_FONT = pygame.font.SysFont("Calibri",50)
+
+# match end after player reach 10 score
+# a round end when ball pass any player
+won_sfx = pygame.mixer.Sound("assets/won.wav")
+ball_hit_sfx = pygame.mixer.Sound("assets/ball_hit.wav")
+
+game_active = True
+
 player_width = 20
 player_height = 150
 player_offset_x = 20
-player_speed = 5
+player_speed = 10
 player_1_turn = True
+player_1_score = 0
+player_2_score = 0
 player_1_rect = pygame.Rect(
     player_offset_x,
     HEIGHT/2-player_height/2,
@@ -25,8 +39,8 @@ player_2_rect = pygame.Rect(
     player_height
 )
 ball_edge_size = 20
-ball_init_vel = 5
-ball_vel = pygame.math.Vector2(ball_init_vel,0)
+ball_init_vel = 7
+ball_vel = Vector2(ball_init_vel,0)
 
 ball_rect = pygame.Rect(
     WIDTH/2-ball_edge_size/2,
@@ -45,12 +59,41 @@ def init_ball():
     ball_vel.rotate_ip(randint(-30,30))
 init_ball()
 
+def reset_round(player_won : int):
+    global player_1_score,player_2_score,player_1_turn
+    player_1_score += player_won == 1
+    player_2_score += player_won == 2
+    player_1_turn = not (player_won == 1)
+    init_ball()
+    won_sfx.play()
+
 def collision_handler():
+    # collide with edge screen
+    global served,ball_vel
     if ball_rect.top + ball_vel.y < 0 :
-        ball_rect.x += ball_vel.x
-        ball_rect.y += ball_rect.y-
         ball_vel.y = -ball_vel.y
-    if ba
+        ball_rect.top = -ball_rect.top
+        ball_hit_sfx.play()
+    elif ball_rect.bottom + ball_vel.y > HEIGHT:
+        ball_vel.y = -ball_vel.y
+        ball_rect.bottom = 2*HEIGHT - ball_rect.bottom
+        ball_hit_sfx.play()
+    
+    if ball_rect.colliderect(player_1_rect):
+        collidex = player_1_rect.right-ball_rect.left
+        ball_rect.centerx += collidex
+        ball_rect.centery += collidex*ball_vel.y/ball_vel.x
+        ball_vel = Vector2(ball_rect.center) - (player_1_rect.centerx - player_height/2,player_1_rect.centery)
+        ball_vel.scale_to_length(ball_init_vel*2)
+        ball_hit_sfx.play()
+    elif ball_rect.colliderect(player_2_rect):
+        collidex = player_2_rect.left-ball_rect.right
+        ball_rect.centerx += collidex
+        ball_rect.centery += collidex*ball_vel.y/ball_vel.x
+        ball_vel = Vector2(ball_rect.center) - (player_2_rect.centerx + player_height/2,player_2_rect.centery)
+        ball_vel.scale_to_length(ball_init_vel*2)
+        ball_hit_sfx.play()
+    
 
 def fixedUpdate():
     keys = pygame.key.get_pressed()
@@ -69,6 +112,19 @@ def fixedUpdate():
 
     collision_handler()
 
+    if ball_rect.left < -50:
+        reset_round(2)
+    elif ball_rect.right > WIDTH+50:
+        reset_round(1)
+
+def draw_score():
+    player_1_score_text = SCORE_FONT.render(str(player_1_score),True,WHITE)
+    player_2_score_text = SCORE_FONT.render(str(player_2_score),True,WHITE)
+    window.blit(player_1_score_text,(WIDTH/2-player_1_score_text.get_width()-10,10))
+    window.blit(player_2_score_text,(WIDTH/2+10,10))
+
+def draw_net():
+    pygame.draw.line(window,WHITE,(WIDTH/2,0),(WIDTH/2,HEIGHT),2)
 
 def draw_player():
     pygame.draw.rect(window,WHITE,player_1_rect)
@@ -79,9 +135,14 @@ def draw_ball():
 
 def drawScreen():
     window.fill('black')
-    draw_player()
-    draw_ball()
-
+    if game_active:
+        draw_player()
+        draw_ball()
+        draw_score()
+        draw_net()
+    else:
+        
+    
 
 if __name__ == "__main__":
     while True:
@@ -89,6 +150,7 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
         fixedUpdate()
         drawScreen()
         pygame.display.flip()
